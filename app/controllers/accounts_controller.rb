9a6e2c47db4181ddf7801da0026a991e3951743a
Jacob Ferguson <jacob@liscio.me>
2024-07-03 15:31:29 -0700

    Cleaning up user/accounts controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -26,27 +26,19 @@ class AccountsController < ApplicationController
   def create
     @account = Account.new(account_params)
 
-    respond_to do |format|
-      if @account.save
-        format.html { redirect_to account_url(@account), notice: "Account was successfully created." }
-        format.json { render :show, status: :created, location: @account }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
-      end
+    if @account.save
+      render :show, status: :created, json: @account
+    else
+      render json: @account.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /accounts/1 or /accounts/1.json
   def update
-    respond_to do |format|
-      if @account.update(account_params)
-        format.html { redirect_to account_url(@account), notice: "Account was successfully updated." }
-        format.json { render :show, status: :ok, location: @account }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
-      end
+    if @account.update(account_params)
+      render :show, status: :created, json: @account
+    else
+      render json: @account.errors, status: :unprocessable_entity
     end
   end
 
@@ -54,10 +46,7 @@ class AccountsController < ApplicationController
   def destroy
     @account.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to accounts_url, notice: "Account was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    render json: { head: "Account deleted!" }
   end
 
   private
